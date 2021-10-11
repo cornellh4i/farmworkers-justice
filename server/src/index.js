760c959733b1c8cd23769293613fa6c590119611
wg237 <miyukigoay@gmail.com>
2021-10-11 16:20:38 -0400

    Incorporated MongoDB into MERN template

--- server/src/index.js
+++ server/src/index.js
@@ -2,4 +2,14 @@ const appName = "Server API";
 const port = process.env.PORT || 8080;
 const createServer = require("./server");
 const server = createServer();
-server.listen(port, () => console.log(`${appName} running on port ${port}!`));
+require("dotenv").config({ path: "./config.env" });
+const dbo = require("./db/conn");
+
+server.listen(port, () => {
+    // perform a database connection when server starts
+    dbo.connectToServer(function (err) {
+        if (err) console.error(err);
+ 
+    });
+    console.log(`${appName} running on port ${port}!`)
+});

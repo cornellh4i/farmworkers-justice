53a68c0fbaed9aaef50de315e1f56e4f621298a6
Ananya Seelam <as2759@cornell.edu>
2021-11-06 14:19:33 -0400

    Adding preprocessing steps

--- server/src/db/conn.ts
+++ server/src/db/conn.ts
@@ -1,5 +1,6 @@
 import { Db, MongoClient as MC, MongoError } from "mongodb";
 import { Callback } from "mongoose";
+import { cursorTo } from "readline";
 
 const { MongoClient } = require("mongodb");
 const ATLAS_URI = process.env.ATLAS_URI;
@@ -7,23 +8,34 @@ const client = new MongoClient(ATLAS_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
- 
+
 var _db: Db;
- 
+
+function pre_process(db: Db) {
+
+  db.collection('main').deleteMany({ FY: { $lt: "2008" } })
+    .then(function () {
+      console.log("processed!")
+    })
+}
+
 module.exports = {
-    connectToServer: function (callback: Callback) {
-        client.connect(function (err: MongoError, db: MC) {
-        // Verify we got a good "db" object
-        if (db)
-        {
-            _db = db.db("naws");
-            console.log("Successfully connected to MongoDB."); 
-        }
-        return callback(err, "Error in connecting to MongoDB");
-            });
-    },
- 
+  connectToServer: function (callback: Callback) {
+    client.connect(function (err: MongoError, db: MC) {
+      // Verify we got a good "db" object
+      if (db) {
+        _db = db.db("naws");
+        console.log("Successfully connected to MongoDB.");
+        pre_process(_db);
+      }
+      return callback(err, "Error in connecting to MongoDB");
+    });
+  },
+
   getDb: function () {
     return _db;
   },
+
+
+
 };

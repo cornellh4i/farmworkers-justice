cfe2bf09ad1470448252e7a672377c277e2368df
wg237 <miyukigoay@gmail.com>
2021-12-03 08:26:45 -0500

    Modified aggregateTimeSeries to output array of dictionaries

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -12,7 +12,7 @@ import { Db } from "mongodb";
  *          EX: [[2010, 2], [2010, 2]]
  */
 
-async function query_val(variable: string, db: Db, filter_key1?: string, filter_value1?: string | number) {
+async function queryVal(variable: string, db: Db, filter_key1?: string, filter_value1?: string | number) {
   var query = {}
   if (typeof filter_key1 !== 'undefined' && typeof filter_value1 !== 'undefined') {
     query = { [filter_key1]: filter_value1 }
@@ -30,7 +30,7 @@ async function query_val(variable: string, db: Db, filter_key1?: string, filter_
   return filtered_array
 }
 
-async function query_two_vals(variable: string, db: Db, filter_key1?: string, filter_value1?: string | number,
+async function queryTwoVals(variable: string, db: Db, filter_key1?: string, filter_value1?: string | number,
   filter_key2?: string, filter_value2?: string | number) {
   var query = {}
   if (typeof filter_key1 !== 'undefined' && typeof filter_value1 !== 'undefined'
@@ -57,35 +57,38 @@ async function query_two_vals(variable: string, db: Db, filter_key1?: string, fi
  * Takes an array and a string variable
  * @param arr is a nested array of lists that look like: [year, value]. EX: [[2008, 0], [2009, 1]]
  * @param variable is a that is being aggregated. EX: GENDER
- * @returns dictionary of years mapped to percentages of how often a variable appears in that year. 
- *          An average is returned for variables: B11, FWRDays, and NUMFEMPL. 
+ * @returns an array of dictionaries where each dictionary element is formatted
+ *          as {year: 2009, value: 0.54}. The value represents the percentage
+ *          of how often a variable appears in that year. 
+ *          An average value is returned for variables: B11, FWRDays, 
+ *          and NUMFEMPL. 
+ *          The dictionaries are arranged in ascending order based on year
  */
-function aggregate_time_series_data(arr: [string, number][], variable: string) {
-  let a_dict = new Map<string, number>();
-  let total_each_year = new Map<string, number>();
-  // Initializing map to have all 0s
-  function get_fy(v: [string, number]) {
-    let yr: string
-    yr = v[0]
-    a_dict.set(yr, 0)
-    total_each_year.set(yr, 0)
+function aggregateTimeSeries(arr: [number, number][], variable: string) {
+  const minYear : number = Math.min.apply(Math, arr.map(function(a) { return a[0]; }))
+  const maxYear : number = Math.max.apply(Math, arr.map(function(a) { return a[0]; }))
+  let output = new Array<{year: number, value: number}>();
+  let total_each_year = new Map<number, number>();
+  let i;
+  for (i = 0; i < maxYear - minYear + 1; i++) {
+    output[i] = {year: minYear + i, value: 0};
+    total_each_year.set(minYear + i, 0)
   }
-  arr.forEach(get_fy, errorFunc)
 
-  function iterateFunc(v: [string, number]) {
-    let yr: string
-    yr = v[0]
-    let value: number
-    value = v[1]
+  function iterateFunc(v: [number, number]) {
+    const yr: number = v[0];
+    const value: number = v[1];
+    const yrIdx: number = yr - minYear;
 
-    if (a_dict.get(yr) !== undefined && !isNaN(value)) {
+    if (!isNaN(value)) {
       if (variable === "B11" || variable === "FWRDays" || variable === "NUMFEMPL") {
-        a_dict.set(yr, a_dict.get(yr)! + value)
-        total_each_year.set(yr, total_each_year.get(yr)! + 1)
+        output[yrIdx].value += value;
+        total_each_year.set(yr, total_each_year.get(yr)! + 1);
       }
-      else if (value === 1 || value === 0) {
-        a_dict.set(yr, a_dict.get(yr)! + value)
-        total_each_year.set(yr, total_each_year.get(yr)! + 1)
+      // Only consider Yes & No answers for the rest of the variables 
+      else if (value === 1 || value === 0) { 
+        output[yrIdx].value += value;
+        total_each_year.set(yr, total_each_year.get(yr)! + 1);
       }
     }
   }
@@ -95,18 +98,18 @@ function aggregate_time_series_data(arr: [string, number][], variable: string) {
   }
 
   arr.forEach(iterateFunc, errorFunc)
-  a_dict.forEach((data, yr) => {
-    a_dict.set(yr, data / total_each_year.get(yr)!)
+  output.forEach((d) => {
+    d.value = d.value / total_each_year.get(d.year)!
   })
-  return a_dict
+  return output
 }
 
-const LATEST_YEAR = "2018";
+const LATEST_YEAR = 2018;
 
-function aggregate_histogram_data(arr: [string, number][]) {
+function aggregateHistogram(arr: [number, number][]) {
   let recent_vals: Array<number> = [];
 
-  function iterateFunc(v: [string, number]) {
+  function iterateFunc(v: [number, number]) {
     let year = v[0]
     if (year == LATEST_YEAR) {
       recent_vals.push(v[1])
@@ -119,12 +122,12 @@ function aggregate_histogram_data(arr: [string, number][]) {
   return recent_vals
 }
 
-function aggregate_donut_chart_data(arr: [string, number][], variable: string, db: Db,) {
+function aggregateDonutChart(arr: [number, number][], variable: string, db: Db,) {
   // This is a list with the number of times each encoding shows up in arr. 
   // The index of the value in the list corresponds to the encoding value. 
   let aggregate_encodings: Array<number> = [];
   let a_dict = new Map<string, number>();
-  async function iterateFunc(v: [string, number]) {
+  async function iterateFunc(v: [number, number]) {
     let year = v[0]
     let query = { Variable: variable, Encoding: v[1] }
     var encoding_descrp = await db.collection('description-code').find(query).toArray();
@@ -148,46 +151,52 @@ module.exports = () => {
 
   router.get('/filterTwo/:variable/:filterKey1/:filterVal1/:filterKey2/:filterVal2', async (req: Express.Request, res: Express.Response) => {
     const dbo = require("./db/conn");
-    const query_result = await query_two_vals(req.params.variable, dbo.getDb(),
+    const query_result = await queryTwoVals(req.params.variable, dbo.getDb(),
       req.params.filterKey1, req.params.filterVal1, req.params.filterKey2, req.params.filterVal2);
     console.log("query result: ", query_result);
-    const output = await aggregate_time_series_data(query_result, req.params.variable);
-    console.log("aggregated result: ", Object.fromEntries(output));
-    res.json({ msg: Object.fromEntries(output) });
+    const output = await aggregateTimeSeries(query_result, req.params.variable);
+    // console.log("aggregated result: ", Object.fromEntries(output));
+    // res.json({ msg: Object.fromEntries(output) });
+    console.log("aggregated result: ", output);
+    res.json({ msg: output });
   });
 
   router.get('/timeSeries/:variable', async (req: Express.Request, res: Express.Response) => {
     const dbo = require("./db/conn");
-    const query_result = await query_val(req.params.variable, dbo.getDb())
+    const query_result = await queryVal(req.params.variable, dbo.getDb())
     console.log("query result: ", query_result);
-    const output = await aggregate_time_series_data(query_result, req.params.variable);
-    console.log("aggregated result: ", Object.fromEntries(output));
-    res.json({ msg: Object.fromEntries(output) });
+    const output = await aggregateTimeSeries(query_result, req.params.variable);
+    // console.log("aggregated result: ", Object.fromEntries(output));
+    // res.json({ msg: Object.fromEntries(output) });
+    console.log("aggregated result: ", output);
+    res.json({ msg: output });
   });
 
   router.get('/timeSeries/:variable/:filterKey/:filterVal', async (req: Express.Request, res: Express.Response) => {
     const dbo = require("./db/conn");
-    const query_result = await query_val(req.params.variable, dbo.getDb(), req.params.filterKey, req.params.filterVal);
+    const query_result = await queryVal(req.params.variable, dbo.getDb(), req.params.filterKey, req.params.filterVal);
     console.log("query result: ", query_result);
-    const output = await aggregate_time_series_data(query_result, req.params.variable);
-    console.log("aggregated result: ", Object.fromEntries(output));
-    res.json({ msg: Object.fromEntries(output) });
+    const output = await aggregateTimeSeries(query_result, req.params.variable);
+    // console.log("aggregated result: ", Object.fromEntries(output));
+    // res.json({ msg: Object.fromEntries(output) });
+    console.log("aggregated result: ", output);
+    res.json({ msg: output });
   });
 
   router.get('/histogram/:variable', async (req: Express.Request, res: Express.Response) => {
     const dbo = require("./db/conn");
-    const query_result = await query_val(req.params.variable, dbo.getDb())
+    const query_result = await queryVal(req.params.variable, dbo.getDb())
     console.log("query result: ", query_result);
-    const output = await aggregate_histogram_data(query_result);
+    const output = await aggregateHistogram(query_result);
     console.log("aggregated result: ", output);
     res.json({ msg: output });
   });
 
   router.get('/donutAggregation/:variable', async (req: Express.Request, res: Express.Response) => {
     const dbo = require("./db/conn");
-    const query_result = await query_val(req.params.variable, dbo.getDb())
+    const query_result = await queryVal(req.params.variable, dbo.getDb())
     console.log("query result: ", query_result);
-    const output = await aggregate_donut_chart_data(query_result, req.params.variable, dbo);
+    const output = await aggregateDonutChart(query_result, req.params.variable, dbo);
     // console.log("aggregated result: ", Object.fromEntries(output));
     res.json({ msg: Object.fromEntries(output) });
   });

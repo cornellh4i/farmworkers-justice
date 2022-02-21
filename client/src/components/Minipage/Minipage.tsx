1e5ad94c805cbdbe8b31bab5da4846670aecaaed
danielbotros <11758+dab463@users.noreply.github.coecis.cornell.edu>
2022-02-21 16:43:08 -0500

    Minipage improved skeleton

--- client/src/components/Minipage/Minipage.tsx
+++ client/src/components/Minipage/Minipage.tsx
@@ -6,23 +6,18 @@ import { getVariablesByCategory } from "./../Homepage/Homepage"
 
 function Minipage() {
     const variablesInCategories = require('./../../local-json/categories.json')
-    // function getVariables(categoryIndex: number) {
-    //     //var variables = []
-    //     var categories = variablesInCategories["categories"]
-    //     // for (let i = 0; i < categories.length; i++) {
-    //     //     var singlecategory = categories[i]
-    //     //     for (let j = 0; j < singlecategory.length; j++) {
-    //     //         variables.push(variablesInCategories["categories"][i][j])
-    //     //     }
-    //     // }
-    //     //console.log(variables)
-    //     return variables
-    // }
-
-    const v = getVariablesByCategory(0)
     return (
         <div>
-            {v.map((variable) => <Dropdown variableName={variable}></Dropdown>)}
+        <Dropdown categoryIndex={0} categoryVariables={getVariablesByCategory(0)} />
+        <Dropdown categoryIndex={1} categoryVariables={getVariablesByCategory(1)} />
+        <Dropdown categoryIndex={2} categoryVariables={getVariablesByCategory(2)} />
+        <Dropdown categoryIndex={3} categoryVariables={getVariablesByCategory(3)} />
+        <Dropdown categoryIndex={4} categoryVariables={getVariablesByCategory(4)} />
+        <Dropdown categoryIndex={5} categoryVariables={getVariablesByCategory(5)} />
+        <Dropdown categoryIndex={6} categoryVariables={getVariablesByCategory(6)} />
+        <Dropdown categoryIndex={7} categoryVariables={getVariablesByCategory(7)} />
+        <Dropdown categoryIndex={8} categoryVariables={getVariablesByCategory(8)} />
+        <Dropdown categoryIndex={9} categoryVariables={getVariablesByCategory(9)} />
         </div>
     )
 

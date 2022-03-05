de9ce35a684e702f209c4a759ed1717e6bb0bf5e
louisvalencia <louissean12@gmail.com>
2022-03-04 19:03:55 -0500

    Limit to 2 filters

--- client/src/components/FilterPanel/FilterPanel.tsx
+++ client/src/components/FilterPanel/FilterPanel.tsx
@@ -8,40 +8,96 @@ import Select from '@mui/material/Select';
 import Chip from '@mui/material/Chip';
 import Stack from '@mui/material/Stack';
 
+
+//handlechange: disable
+//handeldelete: enable
+
 function FilterPanel() {
   const [gender, setGender] = React.useState('');
   const [currstat, setCurrStat] = React.useState('');
   const [flc, setFlc] = React.useState('');
   const [region6, setRegion6] = React.useState('');
 
-  const [filter, setFilter] = React.useState<null | string>(null);
+  const [filter1, setFilter1] = React.useState<null | string[]>(null);
+  const [filter2, setFilter2] = React.useState<null | string[]>(null);
 
   const handleGenderChange = (event: { target: { value: string; }; }) => {
     console.log(event.target.value);
     setGender(event.target.value);
-    setFilter(event.target.value);
+    if(filter1 === null) {
+      setFilter1([event.target.value, "gender"]);
+    } else if (filter2 === null && filter1[1] != "gender") {
+      setFilter2([event.target.value, "gender"]);
+    }
+    
   };
   const handleCurrStatChange = (event: { target: { value: string; }; }) => {
     setCurrStat(event.target.value);
-    setFilter(event.target.value);
+    if(filter1 === null) {
+      setFilter1([event.target.value, "currstat"]);
+    } else if (filter2 === null && filter1[1] != "currstat") {
+      setFilter2([event.target.value, "currstat"]);
+    }
   };
   const handleFlcChange = (event: { target: { value: string; }; }) => {
     setFlc(event.target.value);
-    setFilter(event.target.value);
+    if(filter1 === null) {
+      setFilter1([event.target.value, "flc"]);
+    } else if (filter2 === null && filter1[1] != "flc") {
+      setFilter2([event.target.value, "flc"]);
+    }
   };
   const handleRegion6Change = (event: { target: { value: string; }; }) => {
     setRegion6(event.target.value);
-    setFilter(event.target.value);
+    if(filter1 === null) {
+      setFilter1([event.target.value, "region6"]);
+    } else if (filter2 === null && filter1[1] != "region6") {
+      setFilter2([event.target.value, "region6"]);
+    }
   };
 
-    function handleDelete(){
-      setFilter(null);
-      setGender('');
-      setCurrStat('');
-      setFlc('');
-      setRegion6('');
+    function handleDelete1(){
+      if(filter1![1] === "gender"){
+        setGender('');
+      }
+      else if(filter1![1] === "currstat"){
+        setCurrStat('');
+      }
+      else if(filter1![1] === "flc"){
+        setFlc('');
+      }
+      else{
+        setRegion6('');
+      }
+
+      if(filter2 !== null){
+        setFilter1(filter2);
+        setFilter2(null);
+      }
+      else{
+        setFilter1(null);
+      }
 
 
+      
+    };
+
+    function handleDelete2(){
+      if(filter2![1] === "gender"){
+        setGender('');
+      }
+      else if(filter2![1] === "currstat"){
+        setCurrStat('');
+      }
+      else if(filter2![1] === "flc"){
+        setFlc('');
+      }
+      else{
+        setRegion6('');
+      }
+      setFilter2(null);
+
+      
     };
 
   return (
@@ -122,14 +178,19 @@ function FilterPanel() {
       </Box>
       < div >
 
-{filter===null?
+{filter1===null?
 null:
 <Stack direction="row" spacing={1}>
-        <Chip label={filter} onDelete={handleDelete} />
+        <Chip label={filter1[0]} onDelete={handleDelete1} />
       </Stack>
-
 }
 
+{filter2===null?
+null:
+<Stack direction="row" spacing={1}>
+        <Chip label={filter2[0]} onDelete={handleDelete2} />
+      </Stack>
+}
     </div >
 
     </div>

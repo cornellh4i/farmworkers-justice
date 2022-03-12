26c75620b7c72d4a05a2e4897cacc1f25902e0e2
Dylan Tom <tomd@bxscience.edu>
2022-03-11 22:45:35 -0500

    Fixed major issue with CSV file
    need to remove commas from numbers in datasheet

--- server/src/db/preprocess.ts
+++ server/src/db/preprocess.ts
@@ -3,25 +3,31 @@ import * as dfd from "danfojs-node";
 const latestFY: number = 2020;
 const earliestFY: number = (latestFY + 1) - 10;
 const notNecessaryVariables: string[] = [];
+let df: dfd.DataFrame | dfd.Series;
 
-// paste -d "," ./toCombine/*.csv > combined.csv
+async function preprocess() {
+	let df1 = await dfd.readCSV("./data/NAWS_A2E191.csv")
+					.then((df: dfd.DataFrame) => {return df;});
+	let df2 = await  dfd.readCSV("./data/NAWS_F2Y191.csv")
+					.then((df: dfd.DataFrame) => {return df;});
 
-dfd.readCSV("./combined.csv")
-	.then((df: dfd.DataFrame) => { 
-		console.log(df.shape)
-		df = df.drop({columns: notNecessaryVariables});
-		for (let i = 0; i < df.columns.length; i++) {
-			df = df.asType(df.columns[i], "int32");
-		}
+	df = dfd.concat({dfList : [df1, df2], axis : 1})
+	dfd.toCSV(df, { filePath: "combined.csv" });
+	console.log(df.shape)
+
+		// df = df.drop({columns: notNecessaryVariables});
+		// for (let i = 0; i < df.columns.length; i++) {
+		// 	df = df.asType(df.columns[i], "int32");
+		// }
 		// df = df.replace("", "null");
+		// df = df.asType(df.columns[], "int32");
 		df = df.query(df['FY'].ge(earliestFY) && df['FY'].le(latestFY));
-	 })
+		console.log(df.shape)
+}
 
+preprocess()
 
-// TODO 1: read the csv file
-
-// TODO 2: combine the two sample datasheets by columns
-//let combined = dfd.concat({dfList : [df1, df2], axis : 1})
+// TODO 3: Drop unnecesary variables
 
 // TODO 4: Change type of all entries to int
 // let df_new = df.asType("D", "int32")

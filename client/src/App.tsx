fedd6960a039385e3b41ed2ed915e502993dc658
Jessica Cho <jjc376@cornell.edu>
2022-03-15 13:33:45 -0400

    Uncommented code for line graph route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-// import { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 // import Histogram from '../src/charts/Histogram';
 import Map from '../src/charts/Map';
 // import * as d3 from "d3";
@@ -18,43 +18,44 @@ function App() {
   // const [tableData, setTableData] = useState<{}>({});
   // const [histogramData, setHistogramData] = useState<Array<number>>([]);
   // const [donutData, setdonutData] = useState<{}>({});
-  // const [FLCData, setFLCData] = useState<{}>({});
-  // const [timeSeriesData, setTimeSeriesData] = useState<Array<{}>>([]);
+  const [FLCData, setFLCData] = useState<{}>({});
+  const [timeSeriesData, setTimeSeriesData] = useState<Array<{}>>([]);
 
-  // useEffect(() => {
-  //   getData();
-  // }, []);
+  useEffect(() => {
+    getData();
+  }, []);
 
-  // async function getData() {
-  //   const urlHistogram = `${API_URL}/AGE`;
-  //   const histogramResponse = await fetch(urlHistogram);
-  //   const histogramOut = await histogramResponse.json();
-  //   setHistogramData(histogramOut.data);
+  async function getData() {
+    //   const urlHistogram = `${API_URL}/AGE`;
+    //   const histogramResponse = await fetch(urlHistogram);
+    //   const histogramOut = await histogramResponse.json();
+    //   setHistogramData(histogramOut.data);
 
-  //   const urlTable = `${API_URL}/B01`;
-  //   const tableResponse = await fetch(urlTable);
-  //   const tableOut = await tableResponse.json();
-  //   setTableData(tableOut.data);
+    //   const urlTable = `${API_URL}/B01`;
+    //   const tableResponse = await fetch(urlTable);
+    //   const tableOut = await tableResponse.json();
+    //   setTableData(tableOut.data);
 
-  //   const urlDonut = `${API_URL}/B07`;
-  //   const donutResponse = await fetch(urlDonut);
-  //   const donutOut = await donutResponse.json();
-  //   setdonutData(donutOut.data);
-
-  //   const urlFLC = `${API_URL}/FLC`;
-  //   const FLCResponse = await fetch(urlFLC);
-  //   const FLCOut = await FLCResponse.json();
-  //   setFLCData(FLCOut.data);
-  //   setTimeSeriesData(FLCOut.timeSeriesData)
-  // }
+    //   const urlDonut = `${API_URL}/B07`;
+    //   const donutResponse = await fetch(urlDonut);
+    //   const donutOut = await donutResponse.json();
+    //   setdonutData(donutOut.data);
+    // 
+    const urlFLC = `${API_URL}/FLC`;
+    const FLCResponse = await fetch(urlFLC);
+    const FLCOut = await FLCResponse.json();
+    setFLCData(FLCOut.data);
+    setTimeSeriesData(FLCOut.timeSeriesData)
+  }
 
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage />} />
-        <Route path="/visualizations/:categoryEncoding" element={<Minipage />} />
-        <Route path = "/linegraph" element = {<Line data = {timeSeriesData} width = {500} height = {500} categoryEncoding = {"8"} variableEncoding = {"G01"} variableDescription = {"What was your total income last year in USD?"}/>} />
-        <Route path='/map' element={<Map regionEncoding = {"1"} />} />
+        {/* <Route path="/visualizations/:categoryEncoding" element={<Minipage />} /> */}
+
+        <Route path="/linegraph" element={<Line data={timeSeriesData} width={500} height={500} categoryEncoding={"8"} variableEncoding={"G01"} variableDescription={"What was your total income last year in USD?"} />} />
+        {/* <Route path='/map' element={<Map regionEncoding = {"1"} />} /> */}
         {/* <h3 style={{ marginBottom: "1px", marginLeft: "200px" }}>\
         Respondent Age
       </h3>

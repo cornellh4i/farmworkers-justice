08592e0f68c43722c9548ed359a8e9a449193226
Anusha Choudhury <ac978@cornell.edu>
2022-03-19 20:40:53 -0400

    finished donutChart

--- client/src/charts/donutChart.tsx
+++ client/src/charts/donutChart.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef } from "react";
 import * as d3 from "d3";
+import './donutChart.scss';
 
 interface DonutChartProps {
   data: any,
@@ -7,10 +8,14 @@ interface DonutChartProps {
 
 function DonutChart(props: DonutChartProps) {
   const ref: React.MutableRefObject<null> = useRef(null);
-  const innerRadius = 150
-  const outerRadius = 200
-  const height = 500
-  const width = 500
+  const innerRadius = 250
+  const outerRadius = 300
+  const height = 2000
+  const width = 2000
+  const labelHeight = 18
+  const legendRectSize = 15  
+  const textheight = 18                            
+  const legendSpacing = 4;                                    // NEW
 
   //ceating the pie layout
   const createPie = d3
@@ -33,6 +38,8 @@ function DonutChart(props: DonutChartProps) {
     donutData.push(value)
   }
 
+  const arrSum = donutData.reduce((a,b) => a + b, 0)
+  console.log(arrSum)
   
   const data = createPie(donutData);
   const group = d3.select(ref.current);
@@ -55,9 +62,9 @@ function DonutChart(props: DonutChartProps) {
     .attr("fill", (d, i) => colors(i.toString()));
 
 
-  const text = groupWithUpdate
-    .append("text")
-    .merge(groupWithData.select("text"));
+  // const text = groupWithUpdate
+  //   .append("text")
+  //   .merge(groupWithData.select("text"));
 
   // Want to loop through the keys to make this generalizable
   const keys = Object.keys(props.data);
@@ -65,49 +72,66 @@ function DonutChart(props: DonutChartProps) {
     // get the specific key each time
     return keys[x]
   }
-  // text labels 
-  text
-    .attr("text-anchor", function (d) {
-      return d.startAngle + (d.endAngle - d.startAngle)/2 < Math.PI ?
-        "start":"end";
-    })
-    .attr("alignment-baseline", "middle")
-    .attr("transform", function (d) {
-      var c = createArc.centroid(d),
-        x = c[0],
-        y = c[1],
-        // unsure of this
-        h = Math.sqrt(x * x + y * y);
-      return "translate(" + ((x + 10) / h * 175) + ',' +
-        (y / h * 175) + ")";
-    })
-    .style("fill", "black")
-    .style("font-size", 12)
-    .text(d => formatText(d.index));
 
   
-    const legendG = groupWithUpdate
-      .append("legend")
-      .merge(groupWithData.select("legend"))
-      .attr("transform", function(d,i){
-        return "translate(" + (width - 110) + "," + (i * 15 + 20) + ")";
-      })
-      .attr("class", "legend");   
-    
-    legendG.append("rect")
-      .attr("width", 10)
-      .attr("height", 10)
-      .attr("fill", function(d, i) {
-        return colors(i.toString());
-      });
-    
-    legendG.append("text")
-      .text(function(d){
-        return d.value + "  " + d.data;
-      })
-      .style("font-size", 12)
-      .attr("y", 10)
-      .attr("x", 11);
+  //text labels 
+  // text
+  //   .attr("text-anchor", function (d) {
+  //     return d.startAngle + (d.endAngle - d.startAngle)/2 < Math.PI ?
+  //       "start":"end";
+  //   })
+  //   .attr("alignment-baseline", "middle")
+  //   .attr("transform", function (d) {
+  //     var c = createArc.centroid(d),
+  //       x = c[0],
+  //       y = c[1],
+  //       // unsure of this
+  //       h = Math.sqrt(x * x + y * y);
+  //     return "translate(" + ((x + 10) / h * 175) + ',' +
+  //       (y / h * 175) + ")";
+  //   })
+  //   .style("fill", "black")
+  //   .style("font-size", 12)
+  //   .text(d => formatText(d.index));
+
+  
+ // again rebind for legend
+
+const legend = groupWithUpdate
+    .attr("class", "legend")
+    .merge(groupWithData.select("legend"))
+    // .attr('transform', function(d, i) {                     // NEW
+    //   var height = legendRectSize + legendSpacing;          // NEW
+    //   var offset =  height * colors.domain().length / 2;     // NEW
+    //   var horz = -2 * legendRectSize;                       // NEW
+    //   var vert = i * height - offset;                       // NEW
+    //   return 'translate(' + horz + ',' + vert + ')';        // NEW
+    // });     
+    .attr('transform', 'translate(" + (width - 110) + "," + (i * 15 + 20) + ")'); 
+
+legend.append("rect") // make a matching color rect
+.attr('width', legendRectSize)                          // NEW
+.attr('height', legendRectSize)                         // NEW
+// .attr("width",labelHeight)
+// .attr("height", labelHeight)
+.attr('x', innerRadius + innerRadius/10)
+.attr('y', d => (d.index-1) * labelHeight)
+.attr("fill", function(d, i) {
+  return colors(i.toString());
+})
+.attr('stroke', 'grey')
+.style('stroke-width', '1px');
+
+legend.append("text") // add the text
+// .text(d => formatText(d.index))
+.text(d => formatText(d.index) + " - " + String(((d.data.valueOf()/arrSum)*100).toFixed(2)) + "%")
+.style("font-size", 12)
+.attr('x', d => outerRadius)
+.attr('y', d => (d.index) * labelHeight)
+// .attr('x', legendRectSize + legendSpacing)              // NEW
+// .attr('y', legendRectSize - legendSpacing)              // NEW
+.style('font-family', 'sans-serif')
+.style('font-size', `${textheight}px`);
     
   return (
     <svg width={width} height={height}>

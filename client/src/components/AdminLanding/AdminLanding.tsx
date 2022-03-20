db59525d5c2b5ad1641dc81c141f8abffbabc1b3
bananahz <hrz8@cornell.edu>
2022-03-19 21:04:17 -0400

    handling password input and useState

--- client/src/components/AdminLanding/AdminLanding.tsx
+++ client/src/components/AdminLanding/AdminLanding.tsx
@@ -1,14 +1,30 @@
 import { useParams } from 'react-router-dom';
+import React, { ComponentProps } from 'react';
+import { Input } from 'reactstrap'
+import { useState } from "react";
 
+//https://reactjs.org/docs/forms.html 
 function AdminLanding() {
+  const [getPassword, setPassword] = useState<"">("");
+
+  function handleSubmit(event: any) {
+    // pass value to back end through fetch 
+    setPassword(event.value)
+    console.log(getPassword)
+
+  }
+  function handleChange(event: any) {
+    setPassword(event.value)
+  }
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <label>
         password:
-        <input type="text" name="password" />
+        <input type="text" value={getPassword} name="password" onChange={handleChange} />
       </label>
       <input type="submit" value="Submit" />
     </form>
+
   )
 }
 export default AdminLanding

49cd6c16aa01cd87a8e7797c588cfb6245b6dd5c
bananahz <hrz8@cornell.edu>
2022-03-26 19:19:34 -0400

    fixing enter

--- client/src/components/AdminLanding/AdminLanding.tsx
+++ client/src/components/AdminLanding/AdminLanding.tsx
@@ -15,11 +15,19 @@ function AdminLanding() {
   function checkPassword(inputPassword: string) {
     return inputPassword === ADMIN_PASSWORD
   }
-
+  async function handleKeypress(event: any) {
+    if (event.keyCode === 13) {
+      event.preventDefault();
+      console.log("enter pressedf")
+      handleSubmit(event)
+    }
+  }
   async function handleSubmit(event: any) {
     console.log("password submitted: ", password)
     const haveAccess = checkPassword(password)
     console.log("have access: ", haveAccess)
+
+
     // BASED ON WHETHER PASSWORD IS CORRECT, 
     // IF CORRECT -> ROUTE TO ADMIN UPLOAD (ANOTHER COMPONENT)
     // IF INCORRECT -> PUT A WARNING
@@ -40,7 +48,7 @@ function AdminLanding() {
                 PASSWORD:
               </Grid>
               <Grid item xs={6}>
-                <input type="password" value={password} name="password" placeholder="Enter password" onChange={handleChange} />
+                <input type="password" value={password} name="password" placeholder="Enter password" onChange={handleChange} onKeyPress={handleKeypress} />
               </Grid>
               <Grid item xs={6}>
                 <input type="button" value="Submit" onClick={handleSubmit} />

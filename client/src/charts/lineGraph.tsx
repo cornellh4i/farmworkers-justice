13f25a08830773c9aca45ebeda6fd8659e95fd1c
kevinzhou497 <kevinzhou497@gmail.com>
2022-04-11 17:09:24 -0400

    fixing line graph

--- client/src/charts/lineGraph.tsx
+++ client/src/charts/lineGraph.tsx
@@ -13,12 +13,12 @@ interface LineGraphProp {
 
 const toExclude: string[] = ["B11", "G01", "G03", "FWRDays", "NUMFEMPL"];
 
-function findMinY(data: any){
-  return Math.min.apply(Math, data.map(function(o: any) { return o.value; }));
+function findMinY(data: any) {
+  return Math.min.apply(Math, data.map(function (o: any) { return o.value; }));
 }
 
-function findMaxY(data: any){
-  return Math.max.apply(Math, data.map(function(o: any) { return o.value; }));
+function findMaxY(data: any) {
+  return Math.max.apply(Math, data.map(function (o: any) { return o.value; }));
 }
 
 function LineGraph(props: LineGraphProp) {
@@ -34,24 +34,24 @@ function LineGraph(props: LineGraphProp) {
     .style('margin-left', '100')
     .style('overflow', 'visible');
   const xScale = d3.scaleLinear()
-    .domain((props.data.length !== 0)? 
-      [props.data[0].year, props.data[Object.keys(props.data).length-1].year + 1] : 
+    .domain((props.data.length !== 0) ?
+      [props.data[0].year, props.data[Object.keys(props.data).length - 1].year + 1] :
       [2007, 2018]) //[2007, 2018] is a temp placeholder before props is updated
 
     .range([0, w])
   const yScale = d3.scaleLinear()
-    .domain([findMinY(props.data), findMaxY(props.data)])
+    .domain([.9 * findMinY(props.data), 1.1 * findMaxY(props.data)])
     .range([h, 0])
 
   const generatedScaleLine = d3.line()
     .x((d, i) => xScale(props.data[i].year + 1))  //TODO: THIS IS A TEMPORARY FIX: NOT TOO SURE WHY NEED THE +1 TO ALIGN LINE
     .y((d, i) => yScale(props.data[i].value))
     .curve(d3.curveCardinal)
-  
+
   const xAxis = d3.axisBottom(xScale)
     .ticks(Object.keys(props.data).length)
     // .tickFormat(i => String(i))
-    .tickFormat((d , i) => `${props.data[i].year} - ${props.data[i].year + 1}`)
+    .tickFormat((d, i) => `${props.data[i].year} - ${props.data[i].year + 1}`)
   const yAxis = d3.axisLeft(yScale)
     .ticks(5);
   svg.append('g')
@@ -65,23 +65,23 @@ function LineGraph(props: LineGraphProp) {
     .attr("x", w / 2)
     .attr("y", h + 50)
     .style('font-family', 'Rubik')
-    .text("Years");    
+    .text("Years");
   svg.append("text")
     .attr("class", "y label")
     .attr("text-anchor", "end")
     .attr("y", -60)
-    .attr("x", -w/2 + ((variableDescription.length * 8)/2))
+    .attr("x", -w / 2 + ((variableDescription.length * 8) / 2))
     .attr("dy", ".75em")
     .attr("transform", "rotate(-90)")
     .style('font-family', 'Rubik')
-    .text(variableDescription); 
+    .text(variableDescription);
   svg.append("text")
     .attr("y", -10)
     .style('font-family', 'Rubik')
     .text(() => {
-      if(toExclude.includes(props.variableEncoding)) {
+      if (toExclude.includes(props.variableEncoding)) {
         return variableDescription;
-      } 
+      }
       return `Percentage of survey question: ${variableDescription}`;
     });
 
@@ -92,7 +92,7 @@ function LineGraph(props: LineGraphProp) {
     .attr('fill', 'none')
     .attr('stroke', '#FF820C')
     .attr('stroke-width', '4')
-  
+
   return (
     <svg ref={svgRef}></svg>
   )

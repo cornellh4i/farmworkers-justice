92dbc0f61e8885b89e5d146483eebe5baa8ed1bc
Jessica Cho <jjc376@cornell.edu>
2022-04-23 21:24:45 -0400

    Added percentage text to column chart legend

--- client/src/charts/columnChart.tsx
+++ client/src/charts/columnChart.tsx
@@ -4,7 +4,7 @@ import "@fontsource/rubik";
 
 interface seriesProps {
   type: string,
-  name: string, 
+  name: string,
   data: number[]
 }
 
@@ -13,39 +13,50 @@ interface ColumnChartProps {
 }
 
 function ColumnChart(props: ColumnChartProps) {
+  console.log("column chart data: ", props.data[0].data[0]);
   const options = {
-      chart: {
-        type: 'column'
-      },
-      xAxis: {
-        categories: [''],
-        crosshair: true
-      },
-      yAxis: {
-        min: 0,
-        title: {
-          text: 'Percentage of Respondents'
-        }
-      },
-      plotOptions: {
-        column: {
-          pointPadding: 0.2,
-          borderWidth: 0,
-        }
-      },
-      series: props.data,
-      legend: {
-        labelFormat: '{name} ({value}%)' // TODO
-      },
-    }
+    chart: {
+      type: 'column'
+    },
+    xAxis: {
+      categories: [''],
+      crosshair: true
+    },
+    yAxis: {
+      min: 0,
+      title: {
+        text: 'Percentage of Respondents'
+      }
+    },
+    plotOptions: {
+      column: {
+        pointPadding: 0.2,
+        borderWidth: 0,
+      }
+    },
+    series: props.data,
+    legend: {
+      // labelFormat: '{name} ({point.x}%)' // TODO
+      labelFormatter: function (this: any) {
+        console.log("this: ", this)
+        // return (props.data[0].name + props.data[point.x].data[0].toString());
+        return (this.name + " (" + this.yData[0] + "%)");
+      }
+    },
+  }
 
-    return (
+  return (
     <div>
       <HighchartsReact
         highcharts={Highcharts}
         options={options}
-        /> 
-  </div>
+      />
+    </div>
   )
+}
+
+function calcPercent(ind: number, data: number[]) {
+  var total = data.reduce(function (a, b) { return a + b; });
+  return data[ind] / total * 100;
 }
 export default ColumnChart;

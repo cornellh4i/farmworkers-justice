5586b31e7df012222e06ea20209ac40f9bbc85ba
bananahz <hrz8@cornell.edu>
2022-04-28 18:02:55 -0400

    merge master and caching

--- client/src/charts/multiColumnChart.tsx
+++ client/src/charts/multiColumnChart.tsx
@@ -4,79 +4,79 @@ import "@fontsource/rubik";
 
 
 function MultiColumnChart() {
-    const options = {
-        chart: {
-          type: 'column'
-        },
-        xAxis: {
-          categories: [
-            ''
-          ],
-          crosshair: true
-        },
-        yAxis: {
-          min: 0,
-          title: {
-            text: 'Percentage of Respondents'
-          }
-        },
-        plotOptions: {
-          column: {
-            pointPadding: 0.2,
-            borderWidth: 0
-          }
-        },
-        
-        series: [{
-        type: 'column',
-          name: 'English',
-          data: [20.9]
-      
-        }, {
-            type: 'column',
-          name: 'Spanish',
-          data: [68.6]
-      
-        }, {
-            type: 'column',
-          name: 'Creole',
-          data: [34.9]
-      
-        }, {
-          type: 'column',
-          name: 'Mixtec',
-          data: [17.4]
-      
-        },
-        {
-          type: 'column',
-          name: 'Kanjobal',
-          data: [12.4]
-      
-        },
-        {
-          type: 'column',
-          name: 'Zapotec',
-          data: [10.4]
-      
-        },
-        {
-          type: 'column',
-          name: 'Other',
-          data: [29.4]
-      
-        }
-        
-      ]
+  const options = {
+    chart: {
+      type: 'column'
+    },
+    xAxis: {
+      categories: [
+        ''
+      ],
+      crosshair: true
+    },
+    yAxis: {
+      min: 0,
+      title: {
+        text: 'Percentage of Respondents'
       }
+    },
+    plotOptions: {
+      column: {
+        pointPadding: 0.2,
+        borderWidth: 0
+      }
+    },
 
-      return (
-      <div>
-        <HighchartsReact
-          highcharts={Highcharts}
-          options={options}
-          /> 
+    series: [{
+      type: 'column',
+      name: 'English',
+      data: [20.9]
+
+    }, {
+      type: 'column',
+      name: 'Spanish',
+      data: [68.6]
+
+    }, {
+      type: 'column',
+      name: 'Creole',
+      data: [34.9]
+
+    }, {
+      type: 'column',
+      name: 'Mixtec',
+      data: [17.4]
+
+    },
+    {
+      type: 'column',
+      name: 'Kanjobal',
+      data: [12.4]
+
+    },
+    {
+      type: 'column',
+      name: 'Zapotec',
+      data: [10.4]
+
+    },
+    {
+      type: 'column',
+      name: 'Other',
+      data: [29.4]
+
+    }
+
+    ]
+  }
+
+  return (
+    <div>
+      <HighchartsReact
+        highcharts={Highcharts}
+        options={options}
+      />
     </div>
-    )
+  )
 }
 export default MultiColumnChart;
